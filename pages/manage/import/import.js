353067d95964983b4f9b00e32e93e29c0b85a34b
baofengyv <baofengyv@gmail.com>
2017-06-06 16:33:00 +0800

    完成 导入数据功能

--- pages/manage/import/import.js
+++ pages/manage/import/import.js
@@ -1,1 +1,44 @@
-Page({}) 
+var app = getApp()
+
+Page({
+    importData: function () {
+
+        wx.getClipboardData({
+            success: function (r) {
+
+                try {
+
+                    var data = JSON.parse(r.data)
+
+                    wx.showModal({
+                        title: '现有数据将被覆盖!!',
+                        content: '确定要导入吗？',
+                        success: function (res) {
+                            if (res.confirm) {
+
+                                // 覆盖掉现有的数据
+                                app.DATA = data
+                                app.saveDATA()
+
+                                // 跳转到密码列表页
+                                wx.switchTab({
+                                    url: '/pages/list/list'
+                                })
+
+                            }
+                        }
+                    })
+
+                } catch (e) {
+                    wx.showModal({
+                        title: '无效的JSON字符串',
+                        content: '请检查剪贴板中的数据!',
+                        showCancel: false
+                    })
+                }
+            }
+        })
+
+
+    }
+}) 
